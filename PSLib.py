d4523c3ec72a7f6fd92174962a03236b5abe2345
ismaelvr1999 <ismael.villalpando@alumnos.udg.mx>
2023-09-01 16:55:33 -0600

    Algoritmo genetico Terminado y archivos reorganizados

--- PSLib.py
+++ PSLib.py
@@ -8,25 +8,55 @@ class AlgBio:
         pass
 
     class AlgGen:
-        Individuos = []
+
         def __init__(self) -> None:
             pass
-
         def GenerarInd(self):
-            self.Individuos = [random.randint(0,15) for x in range(0,30)]
-            print(self.Individuos)
-            return self.Individuos
+            Individuos = [random.randint(0,15) for x in range(0,30)]
 
-
-
-
-
-
-# gen = AlgBio.AlgGen()
-
-# gen.GenerarInd()
-# gen.CalcApt()
-
+            return Individuos
+        
+        def Cruza(self,Individuos,tasa_mutacion):
+            nuevaGeneracion = [] 
+            for x in range(0,15):
+                IndRand1 = random.randint(0,29)
+                IndRand2 = random.randint(0,29)
+                PuntoCruzaR = random.randint(0,3)
                 
+                Padre1 = Individuos[IndRand1]
+                Padre2 = Individuos[IndRand2]
+                
+                BinPadr1 = str(bin(Padre1))[2:]
+                BinPadr2 = str(bin(Padre2))[2:]
+                
+                BinPadr1 = BinPadr1.zfill(4)
+                BinPadr2 = BinPadr2.zfill(4)
+
+                Hijo1 = BinPadr2[:PuntoCruzaR] + BinPadr1[PuntoCruzaR:]
+                Hijo2 = BinPadr1[:PuntoCruzaR] +BinPadr2[PuntoCruzaR:]
+
+                print(f"Punto de Cruze: {str(PuntoCruzaR)} Padre 1: {BinPadr1} Padre 2: {BinPadr2}")
+                print(f"Hijo 1: {Hijo1} Hijo 2: {Hijo2}")
+
+                MutacionH1 = self.Mutacion(Hijo1,tasa_mutacion)
+                MutacionH2 = self.Mutacion(Hijo2,tasa_mutacion)
+                print(f"Mutacion Hijo 1: { MutacionH1} MutacionHijo 2: { MutacionH2}")
+
+                nuevaGeneracion.append(int(MutacionH1,2))
+                nuevaGeneracion.append(int(MutacionH2,2))
+                
+            return nuevaGeneracion
+
+        def Mutacion(self,Hijo,tasa_mutacion):
+            HijoMutado = ""
+            for i in range(len(Hijo)):
+                gen = Hijo[i]
+                if random.random() < tasa_mutacion:
+                    if gen == "0":
+                        HijoMutado += "1"
+                    else: HijoMutado += "0"
+                else: HijoMutado +=gen
+            return HijoMutado
+ 
                 
     
--- ParamLib.py
+++ ParamLib.py
@@ -7,94 +7,44 @@ class ParamLib:
         pass
 
     class ParamAlg:
-        Aptitud = {}
-        MasApt = {}
+        MasApt = ()
         Individuos = []
         Nueva_Generacion = []
         tasa_mutacion = 0.1
 
-        def __init__(self,Ind) -> None:
-            self.Individuos = Ind
+        def __init__(self,Algoritmo) -> None:
+            self.AlgoritmoGen = Algoritmo
 
-        def funObj(self):
+        def funObj(self,Individuos):
+            Aptitud = {}
             for x in range(0,30):
-                eval = pow(self.Individuos[x],2)+2
-                self.Aptitud[ eval ] = self.Individuos[x]
+                eval = pow(Individuos[x],2)+2
+                Aptitud[ eval ] = Individuos[x]
 
-            mayor =max(self.Aptitud)
-            self.MasApt[self.Aptitud[mayor]] = mayor
-            print(self.Aptitud)
-            print("Mas apto = Evaluacion: "+str(mayor)+" Genotipo: "+str(self.Aptitud[mayor]))
+            mayor =max(Aptitud)
+            self.MasApt = (Aptitud[mayor],mayor)
+            print(Aptitud)
+            print("Mas apto = Evaluacion: "+str(self.MasApt[1])+" Genotipo: "+str(self.MasApt[0]))
+            return Aptitud
 
-        
-        def Cruza(self):
-            for x in range(0,15):
-                IndRand1 = random.randint(0,29)
-                IndRand2 = random.randint(0,29)
-                PuntoCruzaR = random.randint(0,3)
-                
-                Padre1 = self.Individuos[IndRand1]
-                Padre2 = self.Individuos[IndRand2]
-                
-                BinPadr1 = str(bin(Padre1))[2:]
-                BinPadr2 = str(bin(Padre2))[2:]
-                
-                BinPadr1 = BinPadr1.zfill(4)
-                BinPadr2 = BinPadr2.zfill(4)
-
-                Hijo1 = BinPadr2[:PuntoCruzaR] + BinPadr1[PuntoCruzaR:]
-                Hijo2 = BinPadr1[:PuntoCruzaR] +BinPadr2[PuntoCruzaR:]
-
-                print(f"Punto de Cruze: {str(PuntoCruzaR)} Padre 1: {BinPadr1} Padre 2: {BinPadr2}")
-                print(f"Hijo 1: {Hijo1} Hijo 2: {Hijo2}")
-
-                MutacionH1 = self.Mutacion(Hijo1)
-                MutacionH2 = self.Mutacion(Hijo2)
-                print(f"Mutacion Hijo 1: { MutacionH1} MutacionHijo 2: { MutacionH2}")
-
-                # self.Individuos[IndRand1] = int(Hijo1,2)
-                # self.Individuos[IndRand2] = int(Hijo2,2)
-
-                self.Nueva_Generacion.append(int(Hijo1,2))
-                self.Nueva_Generacion.append(int(Hijo2,2))
-
-            print("Nueva generacion")
+        def producir(self):
+            print("Primera generacion")
+            self.Individuos = self.AlgoritmoGen.GenerarInd()
+            print(self.Individuos)
+            self.funObj(self.Individuos)
+            print("------------Cruza---------------")
+            self.Nueva_Generacion = self.AlgoritmoGen.Cruza(self.Individuos,self.tasa_mutacion)
+            print("--------------Nueva generacion-------------")
             print(self.Nueva_Generacion)
-
-        def Mutacion(self, Hijo):
-            HijoMutado = ""
-            for i in range(len(Hijo)):
-                gen = Hijo[i]
-                if random.random() < self.tasa_mutacion:
-                    if gen == "0":
-                        HijoMutado += "1"
-                    else: HijoMutado += "0"
-                else: HijoMutado +=gen
-
-            return HijoMutado
-                    
-
-
-
-
-
-
-
-
-
+            self.funObj(self.Nueva_Generacion)
 
 
         
         
-gen = AlgBio.AlgGen()
-ind = gen.GenerarInd()
-obj = ParamLib.ParamAlg(ind)
-obj.funObj()
+AlgGen = AlgBio.AlgGen()
+obj = ParamLib.ParamAlg(AlgGen)
+obj.producir()
 
-obj.Cruza()
 
-# BinPadr = str(bin(dec))[2:]
-
-# print(BinPadr)
 
 
